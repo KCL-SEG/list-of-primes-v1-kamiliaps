222783bd328e05e5178373004a261fe799bbe698
kamiliaps <76583821+kamiliaps@users.noreply.github.com>
2023-10-28 06:53:00 +0100

    Update primes.py to generate given amount of prime numbers

--- primes.py
+++ primes.py
@@ -1,6 +1,20 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
-
+def is_prime(number):
+    state = False
+    for i in range (2,number):
+        if number % i == 0:
+            state = False
+            break
+        else:
+            state = True
+    return state
+            
 def primes(number_of_primes):
-    list = []
+    list = [2]
+    count = 2
+    while len(list) < number_of_primes:
+        if is_prime(count):
+            list.append(count)
+        count += 1
     return list
